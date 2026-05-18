833fa4af3f52d2ab255c97f3d8754c7fffe6bfb6
Lê Minh Nhật <leminhnhat1326@gmail.com>
2026-05-18 13:31:28 +0700

    feat: add endpoints and controller logic for organizer event management and pagination

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/organizer/dashboard', [DashboardController::class, 'dashboard']);
 
     Route::prefix('organizer/events')->middleware(['auth:sanctum', 'role:organizer'])->group(function () {
-<<<<<<< HEAD
-        Route::post('', [EventController::class, 'create']);
-        Route::put('{id}', [EventController::class, 'update']);
-        Route::patch('{id}/status', [EventController::class, 'updateStatus']);
-    });
-});
-=======
         Route::get('/', [EventController::class, 'myEvents']);
         Route::get('{id}', [EventController::class, 'myEventShow']);
         Route::post('/', [EventController::class, 'create']);
@@ -33,4 +26,3 @@ Route::middleware('auth:sanctum')->group(function () {
 
 });
 ?>
->>>>>>> e2cde99 (feat(be): add endpoints and controller logic for organizer event management and pagination)
