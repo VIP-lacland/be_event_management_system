0f0958edb9c4fd76c747dacf72e45871caab422e
Nhuw29 <nhu2911206@gmail.com>
2026-05-21 23:56:21 +0700

    feat(auth): add email verification

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Laravel\Sanctum\HasApiTokens; // ← QUAN TRỌNG: Trait của Sanctum
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     // ✅ 2. Dùng trait HasApiTokens (phải có để dùng createToken())
     use HasApiTokens, HasFactory, Notifiable;
@@ -18,6 +19,7 @@ class User extends Authenticatable
         'email',
         'password',
         'role',
+        'email_verified_at',
     ];
 
     protected $hidden = [
